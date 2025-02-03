de0b2af2bab2f2f58597bdc654f047cbb22b3f5d
Sanjaygehlot1 <sanjaygehlot1695@gmail.com>
2025-02-03 14:59:43 +0530

    Rename website to Flixora and update logo implementation

--- src/Components/Logo.jsx
+++ src/Components/Logo.jsx
@@ -3,17 +3,9 @@ import React from 'react'
 function Logo() {
   return (
     <div>
-    <svg xmlns="http://www.w3.org/2000/svg" width="150" height="50" viewBox="0 0 150 50">
-  <circle cx="25" cy="25" r="20" fill="#FF0000" />
+    
 
-  <polygon points="20,15 35,25 20,35" fill="#FFFFFF" />
-
-  <text x="58" y="33" fontFamily="Arial, sans-serif" fontSize="20" fill="#FF0000" fontWeight="bold">
-  Flixora
-  </text>
-</svg>
-
-
+    <img src='/logo.svg' alt='Logo'></img>
 
     </div>
   )
